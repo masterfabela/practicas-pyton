030af9dfbb4bb4d6e48476242624fa545dea0256
masterfabela <fabelaretorcida@gmail.com>
2018-08-17 14:35:43 +0200

    corregido archivo

--- Tutoriales Phyton/Python para todos/Practica1.py
+++ Tutoriales Phyton/Python para todos/Practica1.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env/ python
 print ("Hola mundo")
 
-#TIPOS DE DATOS
+# TIPOS DE DATOS
 
 # Esto e unha cadea
 c = "Hola mundo"
 
-#Esto e un enteiro
+# Esto e un enteiro
 e = 23
 
-#Esto e un long
-l = 23
+# Esto e un long
+long = 23
 
-#Numero en octal
+# Numero en octal
 octal = 0o27
 
-#Numero en Hexadecimal
+# Numero en Hexadecimal
 hexDecimal = 0x3452334
 
-#Numero con decimales
+# Numero con decimales
 real = 23.334223
 
-#Numero con decimales en notacion cientifica
+# Numero con decimales en notacion cientifica
 cientifico = 0.1e-3
 
-#Podese comprobar coa funcion type
+# Podese comprobar coa funcion type
 print(type(c))
 print(type(e))
-print(type(l))
+print(type(long))
 print(octal)
 print(hexDecimal)
 
