d65b42202fb1e231c36eed5e711c8e9972133298
masterfabela <fabelaretorcida@gmail.com>
2018-08-17 14:37:07 +0200

    corregido archivo

--- Tutoriales Phyton/PildorasInformaticas/listas.py
+++ Tutoriales Phyton/PildorasInformaticas/listas.py
@@ -1,44 +1,44 @@
 miLista = ["Maria", 5, True, 78.35]
 
-#Imprime toda la lista
+# Imprime toda la lista
 print(miLista[:])
 
-#Imprime cada elemento
+# Imprime cada elemento
 print(miLista[0])
 print(miLista[1])
 print(miLista[2])
 print(miLista[3])
 
-#El numero se cuenta desde el final al ser negativo
+# El numero se cuenta desde el final al ser negativo
 print(miLista[-2])
 
-#Acceder a una porcion de la lista
+# Acceder a una porcion de la lista
 print(miLista[0:3])
 print(miLista[:3])
 print(miLista[2:])
 
-#Añade un elemento al final de la lista
+# Añade un elemento al final de la lista
 miLista.append("Sandra")
 
-#Incluye un elemento en una posicion concreta
+# Incluye un elemento en una posicion concreta
 miLista.insert(2, "Sandra")
 
-#Concatena la lista con otra
+# Concatena la lista con otra
 miLista.extend(["Jose", "Manuel", "Damian"])
 
-#Indica el indice del elemento seleccionado si
+# Indica el indice del elemento seleccionado si
 # existen repetidos nos devolvera el primero
 print(miLista.index("Jose"))
 
-#Elimina un elemento de la lista
+# Elimina un elemento de la lista
 miLista.remove("Ana")
 
 print(miLista[:])
 
-#Elimina el ultimo elemento de una lista
+# Elimina el ultimo elemento de una lista
 miLista.pop()
 
-#Con el operador + las listas se suman
+# Con el operador + las listas se suman
 miLista2 = ["Sandra", "Lucia"]
 miLista3 = miLista + miLista2
 print(miLista3)
