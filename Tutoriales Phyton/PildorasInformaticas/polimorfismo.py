c6d036aa47e1700b37be1bb514e2891b3cb908cc
masterfabela <fabelaretorcida@gmail.com>
2018-08-17 14:44:01 +0200

    corregido archivo

--- Tutoriales Phyton/PildorasInformaticas/polimorfismo.py
+++ Tutoriales Phyton/PildorasInformaticas/polimorfismo.py
@@ -1,27 +1,33 @@
 class Coche():
-  
-  def desplazamiento(self):
-    print("Me desplazo utilizando cuatro ruedas")
 
-class Moto():
+    @staticmethod
+    def desplazamiento():
+        print("Me desplazo utilizando cuatro ruedas")
 
-  def desplazamiento(self):
-    print("Me desplazo utilizando 2 ruedas")
 
-class Camion():
+class Moto:
 
-  def desplazamiento(self):
-    print("Me desplazo utilizando 6 ruedas")
+    @staticmethod
+    def desplazamiento():
+        print("Me desplazo utilizando 2 ruedas")
 
-#Esta funcion sabe de que objeto se trata y puede ejecutar la
+
+class Camion:
+
+    @staticmethod
+    def desplazamiento():
+        print("Me desplazo utilizando 6 ruedas")
+
+
+# Esta funcion sabe de que objeto se trata y puede ejecutar la
 # funcion que sea mas comv eniente
-def desplazamientoVehiculo(vehiculo):
-  vehiculo.desplazamiento()
+def desplazamiento_vehiculo(vehiculo):
+    vehiculo.desplazamiento()
+
 
 miVehiculo = Moto()
 miVehiculo.desplazamiento()
 miVehiculo2 = Coche()
 miVehiculo2.desplazamiento()
 miVehiculo3 = Camion()
-desplazamientoVehiculo(miVehiculo3)
-
+desplazamiento_vehiculo(miVehiculo3)
